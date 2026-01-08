a33709264d197106d8b66e6bf6a02fac8d4ac29c
SEBAS0078 <sebascruz0502@gmail.com>
2026-01-08 12:38:00 -0500

    initial commit for interactive resizing 1/08/26

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Head from 'next/head';
 import * as React from 'react';
 import '@/lib/env';
 
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import styles from "./(customer)/customer/styles/home.module.css"; 
+import IntResizing  from "./(customer)/customer/components/IntResizing"; 
 
 /**
  * SVGR Support
@@ -26,47 +26,37 @@ export default function HomePage() {
   return (
     <main>
       <Head>
-        <title>Hi</title>
+        <title>Hemly</title>
       </Head>
+      <div className={styles.title}>
+        <h1>Hemly</h1>
+        <p>The best decentralized clothing alteration platform</p>
+      </div>
       <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
-
-          <ButtonLink className='mt-6' href='/components' variant='light'>
+        <div className= {styles.mainContainer}>
+          <div className="resizing">
+            <p>Interactive Resizing Placeholder</p>
+              <IntResizing/>
+          </div>
+          <div className="options">
+            <p>Resizing Options Placeholder</p>
+          </div>
+          <div className="navBar">
+            <p>NavBar Placeholder</p>
+          </div>
+        </div>
+        
+      </section>
+                {/* <ButtonLink className='mt-6' href='/components' variant='light'>
             See all components
           </ButtonLink>
-
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
-            />
-          </UnstyledLink>
 
           <footer className='absolute bottom-2 text-gray-700'>
             © {new Date().getFullYear()} By{' '}
             <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
+              Hemly
             </UnderlineLink>
-          </footer>
-        </div>
-      </section>
+          </footer> */}
     </main>
   );
 }
